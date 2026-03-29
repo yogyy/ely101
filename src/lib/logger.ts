62ce8ddda78b37def48ec5ac238080f4d42114e2
yogyy <yogimr261@gmail.com>
2026-03-29 22:58:20 +0700

    feat: add logger

---
+++ src/lib/logger.ts
@@ -0,0 +1,14 @@
+import pino from "pino";
+
+export const logger = pino({
+  level: "info",
+  transport:
+    process.env.NODE_ENV === "development"
+      ? {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+          },
+        }
+      : undefined,
+});
